c0259a5d94003c3efadd7eab58791466083d8799
jason <rhythmlee@gmail.com>
2018-08-08 14:01:05 +0800

    添加注释

--- netease_music.py
+++ netease_music.py
@@ -1,13 +1,16 @@
 # coding=utf8
 
+# importing
 import requests
 import pymongo
 
+# 引入数据库对象，并创建数据库和文档
 client = pymongo.MongoClient()
 db = client.netease_music
 collection = db.playlist
 col_music = db.music
 
+# 网易云音乐特定请求头
 headers = {
     'Cookie': 'appver=1.5.0.75771',
     'Referer': 'http://music.163.com/',
@@ -15,6 +18,7 @@ headers = {
 }
 
 
+# 获取歌单并写入数据库
 def get_playlist(playlist_id):
     playlist_url = 'http://music.163.com/api/playlist/detail?updateTime=-1&id=' + str(playlist_id)
     playlist_req = requests.get(playlist_url, headers=headers)
@@ -22,11 +26,13 @@ def get_playlist(playlist_id):
     collection.update_one({'result.id': playlist_data['result']['id']}, {'$set': playlist_data}, upsert=True)
 
 
+# 获取歌词并写入数据库
 def get_lyric(music_id):
     music_url = 'http://music.163.com/api/song/lyric?os=pc&lv=-1&kv=-1&tv=-1&id=' + str(music_id)
     try:
         music_req = requests.get(music_url, headers=headers)
         music_data = music_req.json()
+        # 创建字典对象用于存储歌词json
         lrc = {}
         lrc['lyric'] = music_data['lrc']['lyric']
         col_music.update_one({'lyric': lrc['lyric']}, {'$set': lrc}, upsert=True)
@@ -35,10 +41,11 @@ def get_lyric(music_id):
         return
 
 
-def main():
+# 歌单爬取主函数
+def main(keywords):
     url = 'http://music.163.com/api/search/pc'
     json = {
-        's': '程序员',
+        's': keywords,
         'offset': 0,
         'limit': 10,
         'type': 1000
@@ -51,7 +58,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    search_words = raw_input('请输入要搜索的关键词： ')
+    main(search_words)
     music_playlists = collection.find()
     for music_playlist in music_playlists:
         musics = music_playlist['result']['tracks']
